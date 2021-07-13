e3a5dfbeca995363d5d2a2e61a58bf5ffd0fcc9d
pornanan <pornanan.worrasangasilp@exxonmobil.com>
2021-07-13 10:16:47 +0700

    update remove okta

--- src/main/java/com/pwr/cpc/config/SecurityConfig.java
+++ src/main/java/com/pwr/cpc/config/SecurityConfig.java
@@ -47,28 +47,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
  // String OktaRedirect =new String("https://CPC-OKTA.azurewebsites.net/");
   String OktaRedirect =new String("http://localhost:8081/");
 
-    // Create the Okta client registration
-    ClientRegistration oktacientRegistration = ClientRegistration
-            .withRegistrationId("okta")
-            .tokenUri("https://xom-poc.okta.com/oauth2/auspdolvnEZiFfZhu5d6/v1/token")
-            .clientId("0oarprsoofpoIXI4e5d6")
-            .clientSecret("2bnQyMQ0tA7bOWXpPXJnAU_nhyXsuKrFKOIbD0Z8")
-            .scope("openid")
-            .authorizationUri("https://xom-poc.okta.com/oauth2/auspdolvnEZiFfZhu5d6/v1/authorize")
-            .redirectUri(OktaRedirect)
-            .authorizationGrantType(new AuthorizationGrantType("authorization_code"))
-            .build();
-
-
-    ClientRegistrationRepository oktaclientRegistrationRepository = new InMemoryClientRegistrationRepository(oktacientRegistration);
-
-
-
-    OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
-        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(oktaclientRegistrationRepository);
-        successHandler.setPostLogoutRedirectUri(OktaRedirect);
-        return successHandler;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -82,9 +60,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 // After we logout, redirect to root page, by default Spring will send you to /login?logout
                 .and().logout().logoutSuccessUrl("/")
-
-                // RP-initiated logout
-                .and().logout().logoutSuccessHandler(oidcLogoutSuccessHandler())
 
                 // enable OAuth2/OIDC
                 .and().oauth2Login()
